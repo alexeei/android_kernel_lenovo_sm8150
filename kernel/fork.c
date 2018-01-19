3afab28c4c0234e70d9fd1bb068c0a670f70db3c
Oscar Salvador <osalvador@techadventures.net>
2018-01-18 16:33:53 -0800

    mm/page_owner.c: remove drain_all_pages from init_early_allocated_pages
    commit 6bec6ad77fac3d29aed0d8e0b7526daedc964970 upstream.
    
    When setting page_owner = on, the following warning can be seen in the
    boot log:
    
      WARNING: CPU: 0 PID: 0 at mm/page_alloc.c:2537 drain_all_pages+0x171/0x1a0
      Modules linked in:
      CPU: 0 PID: 0 Comm: swapper/0 Not tainted 4.15.0-rc7-next-20180109-1-default+ #7
      Hardware name: Dell Inc. Latitude E7470/0T6HHJ, BIOS 1.11.3 11/09/2016
      RIP: 0010:drain_all_pages+0x171/0x1a0
      Call Trace:
        init_page_owner+0x4e/0x260
        start_kernel+0x3e6/0x4a6
        ? set_init_arg+0x55/0x55
        secondary_startup_64+0xa5/0xb0
      Code: c5 ed ff 89 df 48 c7 c6 20 3b 71 82 e8 f9 4b 52 00 3b 05 d7 0b f8 00 89 c3 72 d5 5b 5d 41 5
    
    This warning is shown because we are calling drain_all_pages() in
    init_early_allocated_pages(), but mm_percpu_wq is not up yet, it is being
    set up later on in kernel_init_freeable() -> init_mm_internals().
    
    Link: http://lkml.kernel.org/r/20180109153921.GA13070@techadventures.net
    Signed-off-by: Oscar Salvador <osalvador@techadventures.net>
    Acked-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Ayush Mittal <ayush.m@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -93,7 +93,6 @@
 #include <linux/cpufreq_times.h>
 #include <linux/simple_lmk.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/scs.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
