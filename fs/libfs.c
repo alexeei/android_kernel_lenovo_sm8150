3490ce2a9684e33ee0d9dc968d40a35b9f0192dc
alexeei <megaxaser@gmail.com>
2020-05-09 16:05:57 +0300

    fix infoleak in simple_attr_read()

--- fs/libfs.c
+++ fs/libfs.c
@@ -802,7 +802,7 @@ int simple_attr_open(struct inode *inode, struct file *file,
 {
 	struct simple_attr *attr;
 
-	attr = kmalloc(sizeof(*attr), GFP_KERNEL);
+	attr = kzalloc(sizeof(*attr), GFP_KERNEL);
 	if (!attr)
 		return -ENOMEM;
 
@@ -842,7 +842,8 @@ ssize_t simple_attr_read(struct file *file, char __user *buf,
 	if (ret)
 		return ret;
 
-	if (*ppos) {		/* continued read */
+	if (*ppos && attr->get_buf[0]) {
+		/* continued read */
 		size = strlen(attr->get_buf);
 	} else {		/* first read */
 		u64 val;
