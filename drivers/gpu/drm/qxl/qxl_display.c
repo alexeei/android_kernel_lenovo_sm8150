dcbfef2ee6713a5521f8f9f8ff0f94a87f8bac62
alexeei <alexeei@yandex.ru>
2020-05-20 03:14:21 -0700

    Revert "drm/qxl: qxl_release use after free"
    This reverts commit 32c3d1da320c7bb8a42d728f799f8886a52ae151.

--- drivers/gpu/drm/qxl/qxl_display.c
+++ drivers/gpu/drm/qxl/qxl_display.c
@@ -533,9 +533,9 @@ static int qxl_primary_apply_cursor(struct drm_plane *plane)
 	cmd->u.set.visible = 1;
 	qxl_release_unmap(qdev, release, &cmd->release_info);
 
-	
+	qxl_push_cursor_ring_release(qdev, release, QXL_CMD_CURSOR, false);
 	qxl_release_fence_buffer_objects(release);
-        qxl_push_cursor_ring_release(qdev, release, QXL_CMD_CURSOR, false);
+
 	return ret;
 
 out_free_release:
@@ -701,9 +701,9 @@ static void qxl_cursor_atomic_update(struct drm_plane *plane,
 	cmd->u.position.y = plane->state->crtc_y + fb->hot_y;
 
 	qxl_release_unmap(qdev, release, &cmd->release_info);
-	
+	qxl_push_cursor_ring_release(qdev, release, QXL_CMD_CURSOR, false);
 	qxl_release_fence_buffer_objects(release);
-        qxl_push_cursor_ring_release(qdev, release, QXL_CMD_CURSOR, false);
+
 	if (old_cursor_bo)
 		qxl_bo_unref(&old_cursor_bo);
 
@@ -747,9 +747,8 @@ static void qxl_cursor_atomic_disable(struct drm_plane *plane,
 	cmd->type = QXL_CURSOR_HIDE;
 	qxl_release_unmap(qdev, release, &cmd->release_info);
 
-	
-	qxl_release_fence_buffer_objects(release);
 	qxl_push_cursor_ring_release(qdev, release, QXL_CMD_CURSOR, false);
+	qxl_release_fence_buffer_objects(release);
 }
 
 static int qxl_plane_prepare_fb(struct drm_plane *plane,
