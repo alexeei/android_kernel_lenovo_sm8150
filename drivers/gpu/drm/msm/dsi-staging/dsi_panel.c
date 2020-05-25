2b7839d6f764e70d01a04d466981b9a410048648
alexeei <alexeei@yandex.ru>
2020-05-25 15:32:37 +0300

    revert pr_debug

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -878,7 +878,7 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 	
  bl_lvl = ea_panel_calc_backlight(bl_lvl);  
 #endif								 
-	/* pr_debug("backlight type:%d lvl:%d\n", bl->type, bl_lvl);  */
+	 pr_debug("backlight type:%d lvl:%d\n", bl->type, bl_lvl);  
 	switch (bl->type) {
 	case DSI_BACKLIGHT_WLED:
 		rc = backlight_device_set_brightness(bl->raw_bd, bl_lvl);
