49835f069e7a1b8e7143e3a423e241667004e513
alexeei <alexeei@yandex.ru>
2020-07-23 22:51:09 +0300

    drm: msm: implement fod handling part1

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -175,6 +175,12 @@ struct drm_panel_esd_config {
 	u32 groups;
 };
 
+#define BRIGHTNESS_ALPHA_PAIR_LEN 2
+struct brightness_alpha_pair {
+	u32 brightness;
+	u32 alpha;
+};
+
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -226,7 +232,9 @@ struct dsi_panel {
 	
 	bool doze_enabled;
 	enum dsi_doze_mode_type doze_mode;
-	bool resend_ea;
+	
+	struct brightness_alpha_pair *fod_dim_lut;
+	u32 fod_dim_lut_count;
 
 	
 };
@@ -351,5 +359,8 @@ int dsi_panel_set_doze_status(struct dsi_panel *panel, bool status);
 
 int dsi_panel_set_doze_mode(struct dsi_panel *panel, enum dsi_doze_mode_type mode);
 
+int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
+
+u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
